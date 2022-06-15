2e46ee9cb7c35dcc3986c098f47882debe81447a
Sebastian Michaelsen <sebastian@michaelsen.io>
2022-06-15 08:04:14 +0200

    Improve check for request

--- Classes/Cache/Frontend/VariableFrontendWithWarmupReservation.php
+++ Classes/Cache/Frontend/VariableFrontendWithWarmupReservation.php
@@ -8,7 +8,6 @@ use Psr\Http\Message\ServerRequestInterface;
 use Smic\PageWarmup\Service\QueueService;
 use Smic\PageWarmup\Service\WarmupReservationService;
 use TYPO3\CMS\Core\Cache\Frontend\VariableFrontend;
-use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class VariableFrontendWithWarmupReservation extends VariableFrontend
@@ -16,12 +15,13 @@ class VariableFrontendWithWarmupReservation extends VariableFrontend
     public function set($entryIdentifier, $variable, array $tags = [], $lifetime = null)
     {
         parent::set($entryIdentifier, $variable, $tags, $lifetime);
+        /** @var ?ServerRequestInterface $request */
+        $request = $GLOBALS['ORIGINAL_REQUEST'] ?? $GLOBALS['TYPO3_REQUEST'] ?? null;
         if (
             !is_array($variable) ||
             !isset($variable['page_id']) ||
             !isset($variable['pageTitleInfo']) ||
-            !isset($GLOBALS['TYPO3_REQUEST']) ||
-            !$GLOBALS['TYPO3_REQUEST'] instanceof ServerRequestInterface ||
+            !$request instanceof ServerRequestInterface ||
             count($tags) === 0
         ) {
             // the cache entry doesn't *look* like a page is being cached => nothing to do
@@ -29,8 +29,6 @@ class VariableFrontendWithWarmupReservation extends VariableFrontend
         }
 
         $warmupReservationService = GeneralUtility::makeInstance(WarmupReservationService::class);
-        /** @var ServerRequest $request */
-        $request = $GLOBALS['ORIGINAL_REQUEST'] ?? $GLOBALS['TYPO3_REQUEST'];
         $warmupReservationService->addReservations($this->getIdentifier(), (string)$request->getUri(), $tags);
     }
 
