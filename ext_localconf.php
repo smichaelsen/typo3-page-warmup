c0b5ec05dce99cb50c6f6ae9288823a07af932e4
milicj <87117603+milicj@users.noreply.github.com>
2023-06-19 15:52:45 +0200

    perf: Remove cache flush bind for queue creation

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,12 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pages']['frontend'] = \Smic\PageWarmup\Cache\Frontend\VariableFrontendWithWarmupReservation::class;
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Smic\PageWarmup\Task\WarmupQueueMakerTask::class] = [
+    'extension' => 'page_warmup',
+    'title' => 'Page Cache Warmup Queue Maker',
+    'additionalFields' => \Smic\PageWarmup\Task\WarmupQueueMakerTask::class,
+];
+
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Smic\PageWarmup\Task\WarmupQueueWorkerTask::class] = [
     'extension' => 'page_warmup',
     'title' => 'Page Cache Warmup Queue Worker',
